edb9ae34e6d5f1254c7428002e5bd22273a0347c
Boma <Bomaiyaye@cmail.carleton.ca>
2022-09-19 00:53:52 -0400

    2nd time

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -11,9 +11,7 @@ public class AddressBook {
     public void addBuddy(String name, String address, int phoneNum){
         addBook.add(new BuddyInfo(name,address,phoneNum));
     }
-    public void printbooks(){
-        System.out.println(addBook);
-    }
+
 
     public void removeBuddy(String name, String address, int phoneNum){
         addBook.remove(new BuddyInfo(name,address,phoneNum));
@@ -21,9 +19,9 @@ public class AddressBook {
 
 
     public static void main(String[] args) {
-       // AddressBook book=new AddressBook();
-       // book.addBuddy("halland","manchester city england",818);
-        //book.printbooks();
+        AddressBook book=new AddressBook();
+        book.addBuddy("halland","manchester city england",818);
+        book.removeBuddy("halland","manchester city england",818);
         System.out.println("AddressBook");
 
     }
