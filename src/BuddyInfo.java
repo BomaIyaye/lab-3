da3ac6c48c7bc1f69eaf251c984f00a4632cb202
Boma <Bomaiyaye@cmail.carleton.ca>
2022-09-26 16:30:49 -0400

    everything

---
+++ src/BuddyInfo.java
@@ -0,0 +1,26 @@
+public class BuddyInfo {
+
+
+    private String name;
+    private String address;
+    private int phoneNum;
+    public String getName() {
+        return name;
+    }
+
+    public BuddyInfo(String name, String address, int phoneNum) {
+        this.name = name;
+        this.address = address;
+        this.phoneNum = phoneNum;
+    }
+    public BuddyInfo() {
+        this.name = "Kevin Debruyne";
+        this.address = "31 Library Road";
+        this.phoneNum = 911;
+    }
+
+    public static void main(String[] args) {
+      System.out.println("Hello world!" );
+      BuddyInfo J= new BuddyInfo();
+      System.out.println("Hello "+J.getName() );
+    }}
